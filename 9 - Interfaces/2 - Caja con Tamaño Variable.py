0a175d328d182b1b5ff4abdb4c64cd2163934056
clovisHilcode <38765104+magnoClovis@users.noreply.github.com>
2023-01-30 16:20:15 +0100

    Caja con Tamaño Variable

---
+++ 9 - Interfaces/2 - Caja con Tamaño Variable.py
@@ -0,0 +1,8 @@
+'''
+Una vez hecha la caja con tamaño fijo, trabajaremos para que el tamaño de ésta
+pueda ajustarse según las necesidades del proyecto.
+
+Para hacerlo necesitaremos tener buen dominio sobre el uso de bucles, condicionales y
+variables de tipo caracter. Se recomienda también el uso de variables para indicar
+las dimensiones de la caja (altura y longitud).
+'''
