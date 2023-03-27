b807a0ad5a813215765f1220927dadfbc69185e4
clovisHilcode <38765104+magnoClovis@users.noreply.github.com>
2023-03-27 18:13:38 +0200

    Tic Tac Toe - Final
    Fin

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -3,7 +3,6 @@ Lo primero que hacemos en cualquier proyecto es la parte visual/interfaz de inte
 Las dos funciones abajo trabajan juntas para generar dicha interfaz, en nuestro caso, generan
 la tabla del juego y la muestran por pantalla.
 """
-import random
 
 # limpiarTabla() crea y limpia la lista de posiciones que almacenan los valores de las jugadas
 def limpiarTabla():
@@ -52,89 +51,56 @@ def pregunta():
     return jugador_1, jugador_2
 
 def jugada(posiciones, jugador):
-    casilla = int(input("¿Dónde quieres jugar? Elija un número entre 1 y 9. "))
+    casilla = int(input(f"¿Dónde quieres poner la {jugador}? Elija un número entre 1 y 9. "))
     while not(casilla >= 1 and casilla <= 9 and posiciones[casilla] == " "):
         if casilla < 1 or casilla > 9:
             print("VALOR INVALIDO!!\nIndique un número entre 1 y 9. ")
         elif posiciones[casilla] != " ":
             print("VALOR INVALIDO!!\nIndique una posición vacía. ")
-        casilla = int(input("¿Dónde quieres jugar? Elija un número entre 1 y 9. "))
+        casilla = int(input(f"¿Dónde quieres poner la {jugador}? Elija un número entre 1 y 9. "))
     posiciones[casilla] = jugador
     return posiciones
 
-def victoria(casilla, jugador):
+def victoria(posiciones):
     ganador = 0
-    if casilla[1] == jugador and casilla[2] == jugador and casilla[3] == jugador:
-        ganador = jugador
-    elif casilla[4] == jugador and casilla[5] == jugador and casilla[6] == jugador:
-        ganador = jugador
-    elif casilla[7] == jugador and casilla[8] == jugador and casilla[9] == jugador:
-        ganador = jugador
+    i = 1
+    while ganador == 0 and i < 8:
+        if posiciones[i] == posiciones[i+1] and posiciones[i] == posiciones[i+2] and posiciones[i] != " ":
+            ganador = posiciones[i]
+        i+=3
 
-    elif casilla[1] == jugador and casilla[4] == jugador and casilla[7] == jugador:
-        ganador = jugador
-    elif casilla[2] == jugador and casilla[5] == jugador and casilla[8] == jugador:
-        ganador = jugador
-    elif casilla[3] == jugador and casilla[6] == jugador and casilla[9] == jugador:
-        ganador = jugador
+    i = 1
+    while ganador == 0 and i < 4:
+        if posiciones[i] == posiciones[i+3] and posiciones[i] == posiciones[i+6] and posiciones[i] != " ":
+            ganador = posiciones[i]
+        i+=1
 
-    elif casilla[1] == jugador and casilla[5] == jugador and casilla[9] == jugador:
-        ganador = jugador
-    elif casilla[3] == jugador and casilla[5] == jugador and casilla[7] == jugador:
-        ganador = jugador
-
-    elif " " not in casilla:
-        ganador = -1
-
+    i = 1
+    while ganador == 0 and i < 4:
+        if i == 1 and posiciones[i] == posiciones[i+4] and posiciones[i] == posiciones[i+8] and posiciones[i] != " ":
+            ganador = posiciones[i]
+        elif i == 3 and posiciones[i] == posiciones[i+2] and posiciones[i] == posiciones[i+4] and posiciones[i] != " ":
+            ganador = posiciones[i]
+        i+=2
     return ganador
 
-if __name__ == "__main__":
-    victorias_1 = 0
-    victorias_2 = 0
-    empates = 0
-    reinicio = 1
-    while reinicio == 1:
-        posiciones = limpiarTabla()
-        tabla = crearTabla(posiciones)
-        print(tabla)
+posiciones = limpiarTabla()
+tabla = crearTabla(posiciones)
+print(tabla)
 
-        jugador_1, jugador_2 = pregunta()
-        print("JUGADOR 1: ", jugador_1)
-        print("JUGADOR 2: ", jugador_2)
+jugador_1, jugador_2 = pregunta()
+print("JUGADOR 1: ", jugador_1)
+print("JUGADOR 2: ", jugador_2)
 
-        ganador = 0
-        i = 0
-        jugadores = [jugador_1, jugador_2]
-        while ganador == 0:
-            posiciones = jugada(posiciones, jugadores[i%2])
-            print(crearTabla(posiciones))
-            ganador = victoria(posiciones, jugadores[i%2])
-            i = i + 1
+ganador = victoria(posiciones)
 
-        if ganador == -1:
-            print("EMPATE")
-            empates += 1
-        else:
-            print("El ganador ha sido", ganador)
-            if ganador == jugador_1:
-                victorias_1 += 1
-            else:
-                victorias_2 += 1
-
-        print("Victorias del jugador 1: ", victorias_1)
-        print("Victorias del jugador 2: ", victorias_2)
-        print("Empates: ", empates)
-        reinicio = int(input("Volver a jugar?\n1 - SI\n2 - NO\n"))
-
-'''
 while ganador == 0:
     posiciones = jugada(posiciones, jugador_1)
     print(crearTabla(posiciones))
-    ganador = victoria(posiciones, jugador_1)
-
+    ganador = victoria(posiciones)
     if ganador == 0:
         posiciones = jugada(posiciones, jugador_2)
         print(crearTabla(posiciones))
-        ganador = victoria(posiciones, jugador_2)
+        ganador = victoria(posiciones)
 
-'''
+
